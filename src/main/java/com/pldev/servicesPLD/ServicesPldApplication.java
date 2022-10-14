4a519ad699925d3cfbfd56ca39d35a06dcfd495a
Pedro Luis Rodriguez Perez <pedroluisrp1998@gmail.com>
2022-10-15 00:47:40 +0200

    Commit inicial
    Subida del proyecto

---
+++ src/main/java/com/pldev/servicesPLD/ServicesPldApplication.java
@@ -0,0 +1,13 @@
+package com.pldev.servicesPLD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesPldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesPldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pldev/servicesPLD/ServicesPldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pldev.servicesPLD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesPldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
